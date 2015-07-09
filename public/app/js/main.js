2899f298d9656e15c4ec6fc497aae3b5216b8eb1
leoxopow <leoxopow@gmail.com>
2015-07-09 11:48:38 +0300

    mainPage

---
+++ public/app/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('.bxslider').bxSlider({
+		auto: true,
+		pagerCustom: '#bx-pager'
+	});
+	$('.bx-category').bxSlider({
+		auto: true,
+		pagerCustom: '#bxpager',
+		controls: false
+	});
+});
